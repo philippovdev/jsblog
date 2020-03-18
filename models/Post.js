d668cd80031caac9c368716ea4c3f16b0fe5c211
Andrew <a@fandb.pro>
2020-03-18 09:12:09 +0300

    Adding post to db and showing them on UI '/post/:id'

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,20 @@
 const postsCollection = require('../db').db().collection("posts")
 const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
-let Post = function(data, userid) {
+let Post = function (data, userid) {
     this.data = data
     this.errors = []
     this.userid = userid
-
 }
 
-Post.prototype.cleanUp = function() {
-    if (typeof(this.data.title) !== 'string') { this.data.title = '' }
-    if (typeof(this.data.body) !== 'string') { this.data.body = '' }
+Post.prototype.cleanUp = function () {
+    if (typeof (this.data.title) !== 'string') {
+        this.data.title = ''
+    }
+    if (typeof (this.data.body) !== 'string') {
+        this.data.body = ''
+    }
 
     // Get rid of any bogus properties
     this.data = {
@@ -21,40 +25,67 @@ Post.prototype.cleanUp = function() {
     }
 }
 
-Post.prototype.validate = function() {
-    if (this.data.title === '') {this.errors.push('You must provide a title')}
-    if (this.data.body === '') {this.errors.push('Post content could not be blank')}
+Post.prototype.validate = function () {
+    if (this.data.title === '') {
+        this.errors.push('You must provide a title')
+    }
+    if (this.data.body === '') {
+        this.errors.push('Post content could not be blank')
+    }
 }
 
-Post.prototype.create = function() {
+Post.prototype.create = function () {
     return new Promise((resolve, reject) => {
         this.cleanUp()
         this.validate()
         if (!this.errors.length) {
-        //    Save post into database
+            //    Save post into database
             postsCollection.insertOne(this.data)
                 .then(() => {
                     resolve()
                 })
                 .catch(() => {
-                this.errors.push('please try again later')
-                reject(this.errors)
-            })
+                    this.errors.push('please try again later')
+                    reject(this.errors)
+                })
         } else {
             reject(this.errors)
         }
     })
 }
 
-Post.findSingleById = function(id) {
+Post.findSingleById = function (id) {
     return new Promise(async (resolve, reject) => {
-        if (typeof(id) !== 'string' || !ObjectID.isValid(id)) {
+        if (typeof (id) !== 'string' || !ObjectID.isValid(id)) {
             reject()
             return
         }
-        let post = await postsCollection.findOne({_id: new ObjectID(id)})
-        if (post) {
-            resolve(post)
+        let posts = await postsCollection.aggregate([
+            {$match: {_id: new ObjectID(id)}},
+            {$lookup: {from: 'users', localField: 'author', foreignField: '_id', as: 'authorDocument'}},
+            {
+                $project: {
+                    title: 1,
+                    body: 1,
+                    createdDate: 1,
+                    author: {$arrayElemAt: ['$authorDocument', 0]}
+                }
+            }
+        ]).toArray()
+
+        // clean up author properties in each post object
+        posts = posts.map(post => {
+            post.author = {
+                username: post.author.username,
+                avatar: new User(post.author, true).avatar
+            }
+            console.log(post)
+            return post
+        })
+
+        if (posts.length) {
+            console.log(posts[0])
+            resolve(posts[0])
         } else {
             reject()
         }
