d2d22046d24667bc1fc1efe70ca3fdc7bcadd1eb
Andrew <a@fandb.pro>
2020-04-03 12:30:03 +0300

    Let users to follow each other

---
+++ controllers/followController.js
@@ -0,0 +1,19 @@
+const Follow = require('../models/Follow')
+
+exports.addFollow = function(req, res) {
+    let follow = new Follow(req.params.username, req.visitorId)
+    follow.create()
+        .then(() => {
+            req.flash('success', `Congratulations, you successfully started to follow ${req.params.user}`)
+            req.session.save(() => {
+                res.redirect(`/profile/${req.params.username}`)
+            })
+        })
+        .catch((errors) => {
+            errors.forEach(error => {
+                req.flash('errors', error)
+            })
+            req.session.save(() => res.redirect('/'))
+        })
+}
+
