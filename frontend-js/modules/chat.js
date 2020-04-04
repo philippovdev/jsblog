ddf64820a0c1daa42f601383aba14125c0a8996a
Andrew <a@fandb.pro>
2020-04-04 22:12:50 +0300

    Chat complete | created dynamic titles for posts and profiles

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,30 +1,48 @@
+import DOMPurify from 'dompurify'
+
 export default class Chat {
     constructor () {
         this.openedYet = false
-        this.chatWrapper = document.querySelector('#chat-wrapper')
-        this.openIcon = document.querySelector('.header-chat-icon')
+        this.chatWrapper = document.querySelector("#chat-wrapper")
+        this.openIcon = document.querySelector(".header-chat-icon")
         this.injectHTML()
-        this.chatField = document.querySelector('#chatField')
-        this.chatForm = document.querySelector('#chatForm')
-        this.closeIcon = document.querySelector('.chat-title-bar-close')
+        this.chatLog = document.querySelector("#chat")
+        this.chatField = document.querySelector("#chatField")
+        this.chatForm = document.querySelector("#chatForm")
+        this.closeIcon = document.querySelector(".chat-title-bar-close")
         this.events()
     }
 
     // Events
     events () {
-        this.chatForm.addEventListener('submit', (e) => {
+        this.chatForm.addEventListener("submit", (e) => {
             e.preventDefault()
             this.sendMessageToServer()
         })
-        this.openIcon.addEventListener('click', () => this.showChat())
-        this.closeIcon.addEventListener('click', () => this.hideChat())
+        this.openIcon.addEventListener("click", () => this.showChat())
+        this.closeIcon.addEventListener("click", () => this.hideChat())
     }
 
     // Methods
     sendMessageToServer () {
         this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value})
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+    <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
+          </div>
+        </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      </div>
+    `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
         this.chatField.value = ''
         this.chatField.focus()
+    }
+
+    hideChat () {
+        this.chatWrapper.classList.remove("chat--visible")
     }
 
     showChat () {
@@ -32,51 +50,42 @@ export default class Chat {
             this.openConnection()
         }
         this.openedYet = true
-        this.chatWrapper.classList.add('chat--visible')
+        this.chatWrapper.classList.add("chat--visible")
+        this.chatField.focus()
     }
 
     openConnection () {
         this.socket = io()
-        this.socket.on('chatMessageFromServer', function (data) {
-            alert(data.message)
+        this.socket.on('welcome', data => {
+            this.username = data.username
+            this.avatar = data.avatar
+        })
+        this.socket.on('chatMessageFromServer', (data) => {
+            this.displayMessageFromServer(data)
         })
     }
 
-    hideChat () {
-        this.chatWrapper.classList.remove('chat--visible')
+    displayMessageFromServer (data) {
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+          <div class="chat-other">
+            <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+            <div class="chat-message"><div class="chat-message-inner">
+              <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+              ${data.message}
+            </div></div>
+          </div>
+    `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
     }
 
     injectHTML () {
         this.chatWrapper.innerHTML = `
-            <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
-            <div id="chat" class="chat-log">
-          
-              <!-- template for your own message -->
-              <div class="chat-self">
-                <div class="chat-message">
-                  <div class="chat-message-inner">
-                    Hello, how are you?
-                  </div>
-                </div>
-                <img class="chat-avatar avatar-tiny" src="https://gravatar.com/avatar/f64fc44c03a8a7eb1d52502950879659?s=128">
-              </div>
-              <!-- end template-->
-              
-              <!-- template for messages from others -->
-              <div class="chat-other">
-                <a href="#"><img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"></a>
-                <div class="chat-message"><div class="chat-message-inner">
-                  <a href="#"><strong>barksalot:</strong></a>
-                  I am doing well. How about you?
-                </div></div>
-              </div>
-              <!-- end template-->
-              
-            </div>
-            
-            <form id="chatForm" class="chat-form border-top">
-              <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
-            </form>
-        `
+        <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
+        <div id="chat" class="chat-log"></div>
+        
+        <form id="chatForm" class="chat-form border-top">
+          <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
+        </form>
+    `
     }
 }
